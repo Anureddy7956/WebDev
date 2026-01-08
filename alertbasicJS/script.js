80daf377ce5211425a608108138328976a0e42b2
Anureddy7956 <4q4947@gmail.com>
2026-01-08 10:24:44 +0530

    javascript document

---
+++ alertbasicJS/script.js
@@ -0,0 +1,9 @@
+console.log(`Hello`);
+console.log(`I like Cakes!!`);
+
+//alert box
+window.alert(`This is an alert!! `);
+window.alert(`I like cakes !! `);
+
+document.getElementById("myh1").textContent =`hey wassup `;
+document.getElementById("mypara").textContent = `how are you been`;
